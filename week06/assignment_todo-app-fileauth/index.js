5f4224046d44cb5d00537995c57a603d8483ba42
jayadeep217 <gaddamjayadeep@gmail.com>
2025-05-17 12:41:46 +0530

    authecticate middleware done.

--- week06/assignment_todo-app-fileauth/index.js
+++ week06/assignment_todo-app-fileauth/index.js
@@ -119,19 +119,15 @@ function authenticateToken(req, res, next) {
   try {
     const authHeader = req.headers["authorization"];
 
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return res
-        .status(401)
-        .json({ error: "Missing or malformed Authorization header" });
+    if (!authHeader) {
+      return res.status(401).json({ error: "Missing Authorization header" });
     }
 
-    const token = authHeader.split(" ")[1];
-
-    const user = jwt.verify(token, JWT_SECRET);
+    const user = jwt.verify(authHeader, JWT_SECRET);
     req.user = user;
     next();
   } catch (error) {
-    return res.status(403).json({ error: "Invalid or expired token" });
+    return res.status(403).json({ error: error });
   }
 }
 
